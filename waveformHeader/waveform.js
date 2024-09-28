1c1e0caa27e3c311b31d20f6e2b6d89eb6d1681c
Christopher Cook <christopherstephencook@gmail.com>
2024-09-28 09:45:21 -0500

    feat: basic banner ani

--- waveformHeader/waveform.js
+++ waveformHeader/waveform.js
@@ -1,25 +1,45 @@
-const waveform = document.getElementById("waveformHeader");
-const waveformSplits = document.getElementsByClassName("waveformSplit");
-console.log(waveformSplits);
+
+// const waveform = document.getElementById("waveformHeader");
+const waveform = document.createElement("div");
+waveform.id = "waveformHeader";
+document.body.appendChild(waveform);
+
+const waveformLeft = document.createElement("div");
+waveformLeft.classList.add("waveformSplit");
+const waveformRight = waveformLeft.cloneNode(true);
+waveform.appendChild(waveformLeft);
+waveform.appendChild(waveformRight);
+
 
 const wavebar = document.createElement("div");
 wavebar.classList.add("wavebar");
-
-waveformSplits[0].innerText = "testing one 2 three?";
+// wavebar.style.height = "100%";
 
 let x = 0;
-let w = 10;
+let w = 70;
+let i = 80;
 
-while (x <= w) {
-    waveform.innerText += x;
-    setTimeout(() => {
-        for (side in waveformSplits) {
-            side.appendChild(wavebar);
+let addWaves = () => {
+    if (x >= w){
+        clearInterval(addWaves);
+    } else {
+        waveformLeft.insertBefore(wavebar.cloneNode(true), waveformLeft.children[0]);
+        waveformRight.appendChild(wavebar.cloneNode(true));
+        let h = 30 + Math.floor(Math.random()*70);
+        waveformLeft.firstChild.style.height = `${h}%`;
+        waveformRight.lastChild.style.height = `${h}%`;
+        x++;
+        if (i < 3){
+            i = 10;
+        } else {
+            i -= 8;
         }
-    }, 500);
-    x++;
-    
-};
+        setTimeout(addWaves, i);
+    }
+}; 
+
+setTimeout(addWaves, i);
+
 
 
 
