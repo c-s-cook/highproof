39325b07304924e61ecb220f741497e4cdce3ca1
Christopher Cook <christopherstephencook@gmail.com>
2024-09-28 10:29:11 -0500

    feat: added sin curve to waveform banner

--- waveformHeader/waveform.js
+++ waveformHeader/waveform.js
@@ -1,5 +1,4 @@
 
-// const waveform = document.getElementById("waveformHeader");
 const waveform = document.createElement("div");
 waveform.id = "waveformHeader";
 document.body.appendChild(waveform);
@@ -13,11 +12,30 @@ waveform.appendChild(waveformRight);
 
 const wavebar = document.createElement("div");
 wavebar.classList.add("wavebar");
-// wavebar.style.height = "100%";
+
 
 let x = 0;
-let w = 70;
+let w = 40;
+
+// wave frequency
+//      - increasing gives more waves
+//      - f = 1 just tapers down to edges, not back up
+let f = 1.5;
+
+//  initial interval delay
 let i = 80;
+
+// minimum height of the bar (associated with the waveform div's y offset)
+let minHeight = 30;
+
+
+let waveHeight = () => {
+    
+    let t = Math.abs(Math.sin(Math.trunc((Math.abs(w - x*f) / w)*100)/100));
+
+    // console.log("t^2: ", t*t);
+    return t*t;
+}
 
 let addWaves = () => {
     if (x >= w){
@@ -25,7 +43,11 @@ let addWaves = () => {
     } else {
         waveformLeft.insertBefore(wavebar.cloneNode(true), waveformLeft.children[0]);
         waveformRight.appendChild(wavebar.cloneNode(true));
-        let h = 30 + Math.floor(Math.random()*70);
+        
+        let h = minHeight 
+                + (waveHeight() * (100-minHeight))
+                + Math.floor(Math.random()*20);
+        
         waveformLeft.firstChild.style.height = `${h}%`;
         waveformRight.lastChild.style.height = `${h}%`;
         x++;
@@ -40,6 +62,8 @@ let addWaves = () => {
 
 setTimeout(addWaves, i);
 
+
+console.log("\n \n redo \n \n");
 
 
 
