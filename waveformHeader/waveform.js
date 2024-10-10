c7f76f2a8d848682ccc91b4e7ac1306756c4ae4b
Christopher Cook <christopherstephencook@gmail.com>
2024-10-10 17:33:24 -0500

    feat/js-imports-css

--- waveformHeader/waveform.js
+++ waveformHeader/waveform.js
@@ -1,3 +1,11 @@
+// append css file
+let head = document.getElementsByTagName('head')[0];
+let css = document.createElement('link');
+css.rel = 'stylesheet';
+css.type = 'text/css';
+css.href = './waveform.css';
+head.appendChild(css);
+
 
 // create & append the main container
 const waveform = document.createElement("div");
