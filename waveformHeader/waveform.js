c5c78088d23c85bffb0983f39d763ef81ae1ccf5
Christopher Cook <christopherstephencook@gmail.com>
2024-10-10 22:52:37 -0500

    fix: now only single wave from left

--- waveformHeader/waveform.js
+++ waveformHeader/waveform.js
@@ -21,10 +21,7 @@ document.body.appendChild(waveformGradientBG);
 // create the Left and Right split boxes
 const waveformLeft = document.createElement("div");
 waveformLeft.classList.add("waveformSplit");
-const waveformRight = waveformLeft.cloneNode(true);
-
 waveform.appendChild(waveformLeft);
-waveform.appendChild(waveformRight);
 
 
 // create the initial, individual bar element
@@ -35,26 +32,26 @@ wavebar.classList.add("wavebar");
 
 let width = window.innerWidth;
 
+
 let wavebarGap;
 let wavebarWidth;
 
 let setWaveBars = () => {
-    console.log("width: ", width);
     if (width < 360){
-        wavebarGap = 2;
-        wavebarWidth = 2;
+        wavebarGap = 5;
+        wavebarWidth = 8;
         changeWaveBars();
     } else if (640 >= width && width >= 360) {
-        wavebarGap = 3;
-        wavebarWidth = 4;
+        wavebarGap = 7;
+        wavebarWidth = 8;
         changeWaveBars();
     } else if (1280 >= width && width > 640) {
-        wavebarGap = 10;
+        wavebarGap = 9;
         wavebarWidth = 10;
         changeWaveBars();
     } else if (width > 1280) {
-        wavebarGap = 15;
-        wavebarWidth = 10;
+        wavebarGap = 14;
+        wavebarWidth = 16;
         changeWaveBars();
     }   
 };
@@ -62,20 +59,14 @@ let setWaveBars = () => {
 let bars;
 
 let changeWaveBars = () => {
-    bars = (width/2)/(wavebarGap + wavebarWidth);
+    bars = (width/1)/(wavebarGap + wavebarWidth);
 
     waveformLeft.style.gap = `${wavebarGap}px`;
-    waveformRight.style.gap = `${wavebarGap}px`;
 
     waveformLeft.style.paddingRight = `${wavebarGap/2}px`;
-    waveformRight.style.paddingLeft = `${wavebarGap/2}px`;
 
-    console.log(wavebarGap, wavebarWidth);
-
-    // waveformLeft.children.style.width = `${wavebarWidth}px`;
     for (let c=0; c < waveformLeft.children.length; c++){
         waveformLeft.children[c].style.width = `${wavebarWidth}px`;
-        waveformRight.children[c].style.width = `${wavebarWidth}px`;
     }
 }
 
@@ -83,26 +74,22 @@ let changeWaveBars = () => {
 setWaveBars();
 changeWaveBars();
 
-
 wavebar.style.width = `${wavebarWidth}px`;
-
-// bars = (width/2)/(wavebarGap + wavebarWidth);
-
-
 
 
 
 window.addEventListener("resize", () => {
     width = window.innerWidth;
     setWaveBars();
-    bars = (width/2)/(wavebarGap + wavebarWidth);
+    bars = (width/1)/(wavebarGap + wavebarWidth);
     setTimeout(addWaves, i);
 })
+
 
 // wave frequency
 //      - increasing gives more waves
 //      - f = 1 just tapers down to edges, not back up
-let f = 3.5;
+let f = 2;
 
 //  initial interval delay
 let i = 80;
@@ -114,25 +101,23 @@ let minHeight = 30;
 
 let x = 0;
 let waveHeight = () => {
-    let t = Math.abs(Math.cos(Math.trunc((Math.abs(bars - x*f) / bars)*100)/100));
-
-    // console.log("t^2: ", t*t);
+    let t = Math.abs(Math.sin(Math.trunc((Math.abs(bars - x*f) / bars)*100)/100));
     return t*t;
 }
+
+
 
 let addWaves = () => {
     if (x >= bars){
         clearInterval(addWaves);
     } else {
-        waveformLeft.insertBefore(wavebar.cloneNode(true), waveformLeft.children[0]);
-        waveformRight.appendChild(wavebar.cloneNode(true));
-        
+        waveformLeft.appendChild(wavebar.cloneNode(true));
+
         let h = minHeight 
                 + (waveHeight() * (80-minHeight))
                 + Math.floor(Math.random()*20);
         
-        waveformLeft.firstChild.style.height = `${h}%`;
-        waveformRight.lastChild.style.height = `${h}%`;
+        waveformLeft.lastChild.style.height = `${h}%`;
         x++;
         if (i < 3){
             i = 10;
@@ -144,9 +129,5 @@ let addWaves = () => {
 }; 
 
 setTimeout(addWaves, i);
-
-
-console.log("\n \n redo \n \n");
-
 
 
